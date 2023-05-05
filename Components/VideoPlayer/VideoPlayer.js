b15d2453020802fe80b391fbe1750a0cce5e1059
prestonator <preston.b.jenkins-1@ou.edu>
2023-05-05 18:56:00 -0500

    🛠️refactor(VideoPlayer) load ReactPlayer dynamically

--- Components/VideoPlayer/VideoPlayer.js
+++ Components/VideoPlayer/VideoPlayer.js
@@ -1,6 +1,15 @@
+// Components/VideoPlayer/VideoPlayer.js
 "use client";
 import React from "react";
-import ReactPlayer from "react-player";
+import dynamic from "next/dynamic";
+
+const ReactPlayer = dynamic(
+	() => import("react-player").then((mod) => mod.default),
+	{
+		ssr: false,
+		loading: () => <div>Loading...</div>,
+	}
+);
 
 const VideoPlayer = ({ url }) => {
 	return (
