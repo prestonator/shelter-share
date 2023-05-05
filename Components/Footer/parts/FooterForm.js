2f33c0db706af0b50a18731b4cdd3184365f360b
prestonator <preston.b.jenkins-1@ou.edu>
2023-05-05 14:26:02 -0500

    Added email templates

--- Components/Footer/parts/FooterForm.js
+++ Components/Footer/parts/FooterForm.js
@@ -1,83 +1,171 @@
 "use client";
-import { useState } from "react";
+import React from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import styles from "../Footer.module.css";
 
 const FooterForm = () => {
-	const [name, setName] = useState("");
-	const [email, setEmail] = useState("");
-	const [message, setMessage] = useState("");
-	const [submitted, setSubmitted] = useState(false);
+	// Initialize our states
+	const [isSubmitting, setIsSubmitting] = React.useState(false);
+	const [isSubmitted, setIsSubmitted] = React.useState(false);
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		console.log("Sending");
-		let data = {
-			name,
-			email,
-			message,
-		};
-		fetch("/api/contact", {
-			method: "POST",
-			headers: {
-				Accept: "application/json, text/plain, */*",
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(data),
-		}).then((res) => {
-			console.log("Response received");
-			console.log(res);
-			if (res.status === 200) {
-				console.log("Response succeeded!");
-				setSubmitted(true);
-				setName("");
-				setEmail("");
-				setMessage("");
-			}
-		});
+	// Yup error message overrides
+	const errMess = {
+		req: "Error: Please fill this out",
+		name: "Error: Name is required",
+		email: "Error: Email is required",
+		phone: "Error: Phone number is required",
+		message: "Error: Message is required",
+	};
+
+	// Our Yup Schema for this form
+	const ContactSchema = yup.object().shape({
+		name: yup.string().label("Full Name").required(errMess.name).min(3).max(20),
+		email: yup
+			.string()
+			.label("Email Address")
+			.required(errMess.email)
+			.email("Invalid Email Address"),
+		phone: yup
+			.string()
+			.label("Phone Number")
+			.required(errMess.phone)
+			.min(8)
+			.max(14),
+		message: yup
+			.string()
+			.label("Message")
+			.required(errMess.message)
+			.min(10)
+			.max(1000),
+	});
+
+	// Destruct useForm() and set our Yup schema as the validation resolver
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(ContactSchema),
+	});
+
+	const submitForm = async (data) => {
+		setIsSubmitting(true);
+		const id = toast.loading("Please wait...", { position: "bottom-center" });
+
+		try {
+			await fetch("/api/contact", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(data),
+			}).then((res) => {
+				console.log("Response received");
+				console.log(res);
+				if (res.status === 200) {
+					console.log("Response succeeded!");
+					setIsSubmitted(true);
+				} else if (!res.ok) {
+					throw new Error("Network response was not ok");
+				} else {
+					alert(res.message);
+				}
+			});
+
+
+			setIsSubmitting(false);
+			toast.update(id, {
+				render: "All is good",
+				type: "success",
+				isLoading: false,
+				position: "bottom-center",
+				autoClose: 5000,
+			});
+
+		} catch (e) {
+			alert("An error occurred. See log for details.");
+			toast.update(id, {
+				render: "Something went wrong",
+				type: "error",
+				isLoading: false,
+				position: "bottom-center",
+			});
+			console.error(e);
+		}
 	};
 
 	return (
-		<form className={styles.footerForm}>
-			<div>
-				<input
-					className={styles.input}
-					placeholder="Email"
-					type="email"
-					onChange={(e) => {
-						setEmail(e.target.value);
-					}}
-				/>
-				<input
-					className={styles.input}
-					placeholder="Full Name"
-					type="text"
-					onChange={(e) => {
-						setName(e.target.value);
-					}}
-				/>
-			</div>
-			<div>
-				<textarea
-					className={styles.input}
-					placeholder="Type your Message"
-					type="text"
-					onChange={(e) => {
-						setMessage(e.target.value);
-					}}
-				/>
-			</div>
-			<div>
-				<button
-					className={styles.button}
-					type="submit"
-					onClick={(e) => {
-						handleSubmit(e);
-					}}
+		<>
+			<ToastContainer />
+			{!isSubmitted ? (
+				<form
+					className={styles.footerForm}
+					onSubmit={handleSubmit((data) => submitForm(data))}
 				>
-					Send
-				</button>
-			</div>
-		</form>
+					<div>
+						<input
+							className={styles.input}
+							type="text"
+							placeholder="John Doe"
+							{...register("name", { required: true })}
+						/>
+						<p>{errors.name?.message}</p>
+					</div>
+					<div>
+						<div>
+							<input
+								className={styles.input}
+								type="email"
+								placeholder="john@example.com"
+								{...register("email", { required: true })}
+							/>
+							<p>{errors.email?.message}</p>
+						</div>
+						<div>
+							<input
+								className={styles.input}
+								type="tel"
+								placeholder="(555) 555-5555"
+								{...register("phone", {
+									required: true,
+									min: 8,
+									maxLength: 14,
+								})}
+							/>
+							<p>{errors.phone?.message}</p>
+						</div>
+					</div>
+					<div>
+						<textarea
+							className={styles.input}
+							placeholder="Please type your message..."
+							{...register("message", { required: true })}
+						/>
+						<p>{errors.message?.message}</p>
+					</div>
+					<div>
+						<input
+							className={styles.button}
+							type="submit"
+							value={isSubmitting ? "Sending..." : "Send Message"}
+							disabled={isSubmitting}
+						/>
+					</div>
+				</form>
+			) : (
+				<>
+					<h3 className={styles.submitHeader}>Thank you!</h3>
+					<p className={styles.submitBody}>
+						Your message has been received. Please check your email for
+						confirmation.
+					</p>
+				</>
+			)}
+		</>
 	);
 };
 
--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,30 +1,72 @@
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
-export async function POST(request) {
-	const res = await request.json();
+// Config
+const mailConfig = {
+	host: "smtp.gmail.com",
+	port: 465, // or 587
+	secure: true, // true for 465, false for other ports
+	auth: {
+		user: "shelter.share.webmaster@gmail.com",
+		pass: process.env.WEBMASTER_PASSWORD,
+	},
+};
 
-	const { name, email, message } = res;
+const adminEmail = "Robert @ Shelter Share <shelter.share.webmaster@gmail.com>";
 
-	const transporter = nodemailer.createTransport({
-		port: 465,
-		host: "smtp.gmail.com",
-		auth: {
-			user: "prestonator4000@gmail.com",
-			pass: "",
-		},
-		secure: true,
-	});
+// Function for grabbing template files
+async function getPubFile(file) {
+	const res = await fetch(`https://shelter-share.com/emailTemplates/${file}`);
 
-	const mailData = {
-		from: email,
-		to: "preston@eltonjenkinslaw.com",
-		subject: `Message From ${name}`,
-		text: message + " | Sent from: " + email,
-		html: `<div>${message}</div><p>Sent from: ${email}</p>`,
+	if (!res.ok) {
+		throw new Error("Network response was not ok");
+	}
+
+	const data = await res.text();
+	return data;
+}
+
+export async function POST(req) {
+	const res = await req.json();
+	const { name, email, message, phone } = res;
+	// Create our Nodemailer transport handler
+	let transporter = nodemailer.createTransport(mailConfig);
+
+	// Fetch our template files
+	const template = await getPubFile("template.html");
+	const custHtml = await getPubFile("customer.html");
+	const adminHtml = await getPubFile("admin.html");
+	const custTxt = await getPubFile("customer.txt");
+	const adminTxt = await getPubFile("admin.txt");
+
+	// Format our recipient email address
+	const recipEmail = `${name} <${email}>`;
+
+	// Format our customer-bound email from received form data
+	let sendHtml = template
+		.replace("%BODY%", custHtml)
+		.replace("%NAME%", name)
+		.replace("%EMAIL%", email)
+		.replace("%PHONE%", phone)
+		.replace("%MESSAGE%", message);
+
+	let sendTxt = custTxt
+		.replace("%NAME%", name)
+		.replace("%EMAIL%", email)
+		.replace("%MESSAGE%", message);
+
+	// Customer Mail Data
+	const custMailData = {
+		from: adminEmail,
+		to: recipEmail, // list of receivers
+		subject: "Message Received ✔", // Subject line
+		text: sendTxt, // plain text body
+		html: sendHtml, // html body
 	};
 
-	transporter.sendMail(mailData, (error, info) => {
+
+	// Send our customer-bound email
+	transporter.sendMail(custMailData, (error, info) => {
 		if (error) {
 			console.log(error);
 			res.status(500).send("Error sending message");
@@ -34,7 +76,39 @@ export async function POST(request) {
 		}
 	});
 
+
+	sendHtml = template
+		.replace("%BODY%", adminHtml)
+		.replace("%NAME%", name)
+		.replace("%EMAIL%", email)
+		.replace("%PHONE%", phone)
+		.replace("%MESSAGE%", message);
+
+	sendTxt = adminTxt
+		.replace("%NAME%", name)
+		.replace("%EMAIL%", email)
+		.replace("%PHONE%", phone)
+		.replace("%MESSAGE%", message);
+
+	// Admin Mail Data
+	const adminMailData = {
+		from: recipEmail,
+		to: adminEmail, // list of receivers
+		subject: `New Message From ${name}`, // Subject line
+		text: sendTxt, // plain text body
+		html: sendHtml, // html body
+	};
+
+	transporter.sendMail(adminMailData, (error, info) => {
+		if (error) {
+			console.log(error);
+			res.status(500).send("Error sending message");
+		} else {
+			console.log(`Email sent: ${info.response}`);
+			res.status(200).send("Message sent");
+		}
+	});
+
+
 	return NextResponse.json({ res });
 }
-
-
