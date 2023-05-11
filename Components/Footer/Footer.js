6223cf1a32a0da7bf373f6e0bbe9e74dc685342e
prestonator <preston.b.jenkins-1@ou.edu>
2023-05-11 13:29:40 -0500

    🛠️refactor(footer) change social buttons id to class name

--- Components/Footer/Footer.js
+++ Components/Footer/Footer.js
@@ -18,7 +18,7 @@ const Footer = async () => {
 					<a
 						href="https://www.linkedin.com/in/robert-washington-7855984a"
 						target="_blank"
-						id="linkedin-btn"
+						className="linkedin-btn social-btn"
 						aria-label="Click this button to find out more about Shelter Share's LinkedIn page."
 					>
 						<BsLinkedin />
@@ -26,7 +26,7 @@ const Footer = async () => {
 					<a
 						href="https://www.instagram.com/sheltersharewithme/?igshid=YmMyMTA2M2Y%3D"
 						target="_blank"
-						id="instagram-btn"
+						className="instagram-btn social-btn"
 						aria-label="Click this button to find out more about Shelter Share's Instagram page."
 					>
 						<BsInstagram />
@@ -34,7 +34,7 @@ const Footer = async () => {
 					<a
 						href="https://www.facebook.com/ShelterShareOklahoma?mibextid=LQQJ4d"
 						target="_blank"
-						id="facebook-btn"
+						className="facebook-btn social-btn"
 						aria-label="Click this button to find out more about Shelter Share's Facebook page."
 					>
 						<BsFacebook />
